a57a6ffc0fee2a2bebe7b474ac485403f6e81538
Renato Machado <81891866+renatomachadoo@users.noreply.github.com>
2024-07-10 17:55:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+//EX 1
+  // alert("Hello World")
+
+//EX 2
+  // let numberOne = 2
+  // let numberTwo = 4
+  // alert(numberOne + numberTwo)
+
+//EX 3
+  // let value = "a"
+  // if(typeof(value) == "number"){
+  //   alert("É um número")
+  // }else{
+  //   alert("Não é um número")
+  // }
+
+//EX 4
+// let value = "a"
+// if(typeof(value) == "string"){
+//   alert("É uma string")
+// }else{
+//   alert("Não é uma string")
+// }
+
+//EX 5
+// let value = true
+// if(typeof(value) == "boolean"){
+//   alert("É um booleano")
+// }else{
+//   alert("Não é um booleano")
+// }
+
+//EX 6
+// let numberOne = 2
+// let numberTwo = 4
+// alert(numberOne - numberTwo)
+
+//EX 7
+// let numberOne = 2
+// let numberTwo = 4
+// alert(numberOne * numberTwo)
+
+//EX 8
+// let numberOne = 2
+// let numberTwo = 4
+// alert(numberOne / numberTwo)
+
+//EX 9
+// let number = 23
+// if(number%2 == 0){
+//   alert("É um número par")
+// }else{
+//   alert("Não é um número par")
+// }
+
+//EX 10
+let number = 23
+if(number%2 != 0){
+  alert("É um número impar")
+}else{
+  alert("Não é um número impar")
+}
